503121ce66eb16a45ec45069354642f85ff08989
romain-gfn <105062023+romain-gfn@users.noreply.github.com>
2023-03-20 14:57:08 +0100

    TP 04-05 : 3

---
+++ NoteBook/Stockage/BaseDao.cs
@@ -0,0 +1,33 @@
+﻿using Metier;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stockage
+{
+    public abstract class BaseDao
+    {
+        private SQLiteConnection connection;
+        
+        public void ExecuteNonQuery(string req)
+        {
+            connection.Open();
+            var com = connection.CreateCommand();
+            com.CommandText = req;
+            com.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public SQLiteDataReader ExecuteQuery(string req) 
+        {
+            connection.Open();
+            var com = new SQLiteCommand(req, connection);
+            return com.ExecuteReader(CommandBehavior.CloseConnection);
+        }
+    }
+}
